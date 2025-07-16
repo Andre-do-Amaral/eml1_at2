94d680d4de04ff8868a78d44d2f30582e620f0c4
Andre Amaral <dre_0397@hotmail.com>
2025-07-15 21:06:13 -0300

    Pega a parada ai Jefferson

--- src/main.py
+++ src/main.py
@@ -11,40 +11,49 @@ import mlflow
 import os
 
 
+ml_manager = MLFlowManager()
 # Função para registrar experimento no MLflow
-def mlflow_register():
+def mlflow_register(model_name="decision_tree_model", stage="Staging", experiment_name="Experimento_ML"):
     ml_manager = MLFlowManager()
-    ml_manager.start_experiment("Experimento_ML")
+    ml_manager.mlflow_register(model_name, stage, experiment_name)
+    #ml_manager.start_experiment("Experimento_ML")
+    #with ml_manager.start_run():
     print("Experimento registrado no MLflow.")
-    mlflow.end_run()
+    #ml_manager.end_run()
+
+
+
 
 
 # Função para treinar o modelo
-def train(X_train, y_train):
-    model = ModelFactory.create_model("decision_tree", max_depth=5)
+def train(X_train, y_train, model = 'decision_tree', params = {'max_depth':5}):
+    model = ModelFactory.create_model(model, params)
     model = train_model(model, X_train, y_train)
     print("Passou do treino")
     ml_manager = MLFlowManager()
     print("Conectou com o MLFLOW")
-    ml_manager.start_experiment("Experimento_ML")
-    print("Start no experimento")
+    #ml_manager.start_experiment("Experimento_ML")
+    #print("Start no experimento")
+    #with ml_manager.start_run():
     ml_manager.log_params({"max_depth": 5})
     print("Modelo treinado e registrado no MLflow.")
-    mlflow.end_run()
+    #ml_manager.end_run()
     return model  # retornar o modelo treinado.
 
 
 # Função para avaliar o modelo
-def evaluate(model, X_test, y_test):
+def evaluate(model, X_test, y_test, X_train):
     report = evaluate_model(model, X_test, y_test)
     print("Relatório de Avaliação:")
     print(report)
 
     ml_manager = MLFlowManager()
-    ml_manager.start_experiment("Experimento_ML")
+    #ml_manager.start_experiment("Experimento_ML")
+    #with ml_manager.start_run():
     ml_manager.log_metrics(report["weighted avg"])
+    name_model = "decision_tree_model"
     print("Avaliação registrada no MLflow.")
-    mlflow.end_run()
+    #ml_manager.end_run()
 
 
 # Função para realizar inferência
@@ -54,10 +63,11 @@ def predict(model, X_predict):
     print(predictions)
 
     ml_manager = MLFlowManager()
-    ml_manager.start_experiment("Experimento_ML")
+    #ml_manager.start_experiment("Experimento_ML")
+    #with ml_manager.start_run():
     ml_manager.log_text(str(predictions), "predictions.txt")
     print("Previsões registradas no MLflow.")
-    mlflow.end_run()
+    #ml_manager.end_run()
 
 
 # CLI para gerenciar os comandos
@@ -83,21 +93,85 @@ def main():
     )
     print("Dados Pre-processados")
     args = parser.parse_args()
-
+####### POSSIBILIDADE 1:
     if args.command == "mlflow_register":
-        mlflow_register()
+        ml_manager.start_experiment('Registro mlflow')
+        with ml_manager.start_run():
+            mlflow_register()
     elif args.command == "train":
         print("CHEGOU AQUI")
-        model = train(X_train, y_train)
-        print("PASSOU AQUI")
+        ml_manager.start_experiment('Treino')
+        with ml_manager.start_run():
+            model = train(X_train, y_train)
+            print("PASSOU AQUI")
     elif args.command == "evaluate":
-        model = train(X_train, y_train)  # treina o modelo antes de avaliar.
-        evaluate(model, X_test, y_test)
+        ml_manager.start_experiment('Evaluate')
+        with ml_manager.start_run():
+            model = train(X_train, y_train)  # treina o modelo antes de avaliar.
+            evaluate(model, X_test, y_test, X_train)
+            name_model = "decision_tree_model"
+            ml_manager.log_model(model, name_model, input_example=X_train, signature = model.predict(X_train))
     elif args.command == "predict":
-        model = train(X_train, y_train)  # treina o modelo antes de prever.
-        predict(model, X_test)  # usa X_test para prever como exemplo.
+        ml_manager.start_experiment('Predição')
+        with ml_manager.start_run():
+            model = train(X_train, y_train)  # treina o modelo antes de prever.
+            predict(model, X_test)  # usa X_test para prever como exemplo.
+            name_model = "decision_tree_model"
+            ml_manager.log_model(model, name_model, input_example=X_train, signature=model.predict(X_train))
+    print("Foi até o fim")
+"""
+####### POSSIBILIDADE 2:
+    experimento = 'EXPERIMENTO MLFLOW'
+    ml_manager.start_experiment(experimento)
+    if args.command == "mlflow_register":
+        #ml_manager.start_experiment('Registro mlflow')
+        with ml_manager.start_run():
+            mlflow.set_tag("etapa", "registro")
+            mlflow_register()
+    elif args.command == "train":
+        print("CHEGOU AQUI")
+        #ml_manager.start_experiment('Treino')
+        with ml_manager.start_run():
+            mlflow.set_tag("etapa", "Treino")
+            model = train(X_train, y_train)
+            print("PASSOU AQUI")
+    elif args.command == "evaluate":
+        #ml_manager.start_experiment('Evaluate')
+        with ml_manager.start_run():
+            mlflow.set_tag("etapa", "Evaluate")
+            model = train(X_train, y_train)  # treina o modelo antes de avaliar.
+            evaluate(model, X_test, y_test)
+            ml_manager.log_model(model, name_model, input_example=X_train, signature=model.predict(X_train))
+            mlflow_register(model_name=name_model, experiment_name=experimento)
+            
+    elif args.command == "predict":
+        #ml_manager.start_experiment('Predição')
+        with ml_manager.start_run():
+            mlflow.set_tag("etapa", "Predict")
+            model = train(X_train, y_train)  # treina o modelo antes de prever.
+            predict(model, X_test)  # usa X_test para prever como exemplo.
+            ml_manager.log_model(model, name_model, input_example=X_train, signature=model.predict(X_train))
     print("Foi até o fim")
 
+    # load model que está no mlflow com base em experimento
+
+    client = mlflow.tracking.MlflowClient()
+    experiment = client.get_experiment_by_name("Experimento_ML")
+    runs = client.search_runs(experiment.experiment_id, order_by=["attributes.start_time DESC"], max_results=1)
+
+    if runs:
+        latest_run = runs[0]
+        run_id = latest_run.info.run_id
+        model_uri = f"runs:/{run_id}/{model_name}"
+        loaded_model = mlflow.pyfunc.load_model(model_uri)
+
+    # Load model que está no mlflow que está registrado
+    model_name = "DecisionTreeModel"
+    stage = "Production"  # ou "Staging", "Archived"
+
+    model_uri = f"models:/{model_name}/{stage}"
+    loaded_model = mlflow.pyfunc.load_model(model_uri)
+"""
 
 if __name__ == "__main__":
     print("INICIO")
--- src/utils/mlflow_utils.py
+++ src/utils/mlflow_utils.py
@@ -1,6 +1,9 @@
 import mlflow
 from mlflow import sklearn
 import logging
+from mlflow.models.signature import infer_signature
+from mlflow.tracking import MlflowClient
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,7 +21,61 @@ class MLFlowManager:
                 logging.error(f"Error initializing MLFlowManager: {e}")
                 raise
         return cls._instance
+    
+    def log_model(self, model, model_name, input_example=None, signature=None):
+        logging.info(f"Logging model: {model_name}")
+        try:
+            if signature is None and input_example is not None:
+                signature = infer_signature(input_example)
+            sklearn.log_model(model, model_name, signature=signature, input_example=input_example)
+            logging.info("Model logged successfully.")
+        except Exception as e:
+            logging.error(f"Error logging model: {e}")
+            raise
 
+    def mlflow_register(model_name="decision_tree_model", stage="Staging", experiment_name="Experimento_ML"):
+    
+        client = MlflowClient()
+
+        # Pega o experimento
+        experiment = client.get_experiment_by_name(experiment_name)
+        runs = client.search_runs(experiment.experiment_id, order_by=["attributes.start_time DESC"], max_results=1)
+
+        if not runs:
+            print("Nenhum run encontrado.")
+            return
+
+        latest_run = runs[0]
+        run_id = latest_run.info.run_id
+        model_uri = f"runs:/{run_id}/{model_name}"
+
+        # Registra o modelo
+        try:
+            client.create_registered_model(model_name)
+        except:
+            pass  # já existe
+
+        model_version = client.create_model_version(
+            name=model_name,
+            source=model_uri,
+            run_id=run_id,
+        )
+
+        # Move para o estágio desejado
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage=stage
+        )
+
+        print(f"Modelo registrado como '{model_name}' na versão {model_version.version} e movido para o estágio '{stage}'.")
+
+    def start_run(self):
+        return mlflow.start_run()
+    
+    def end_run(self):
+        return mlflow.end_run()
+    
     def start_experiment(self, experiment_name):
         logging.info(f"Starting experiment: {experiment_name}")
         try:
@@ -48,14 +105,14 @@ class MLFlowManager:
             logging.error(f"Error logging params: {e}")
             raise
 
-    def log_model(self, model, model_name):
-        logging.info(f"Logging model: {model_name}")
-        try:
-            sklearn.log_model(model, model_name)
-            logging.info("Model logged successfully.")
-        except Exception as e:
-            logging.error(f"Error logging model: {e}")
-            raise
+   # def log_model(self, model, model_name):
+   #     logging.info(f"Logging model: {model_name}")
+   #     try:
+   #         sklearn.log_model(model, model_name)
+   #         logging.info("Model logged successfully.")
+   #     except Exception as e:
+   #         logging.error(f"Error logging model: {e}")
+   #         raise
 
     def check_connection(self):
         logging.info("Checking MLflow connection...")
